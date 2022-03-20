07861efe6c10f26d6c0602f178a916483de9ae54
Nishat Sanjida <56416902+sadia1993-design@users.noreply.github.com>
2022-03-21 00:52:52 +0600

    column increased in user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('ip_address')->nullable();
             $table->rememberToken();
             $table->timestamps();
 
